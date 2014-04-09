3ac2221efa25320e57ad376bf5ead2cbddc54269
Ryan Morrison <ryan.r.morrison@me.com>
2014-04-09 15:55:59 -0600

    Add site data and modified scripts

---
+++ r_code/2c-historical_cufa150_processing copy.R
@@ -0,0 +1,158 @@
+#### Hydrologic Data Processessing ####
+library(xts)
+library(prob)
+library(TTR)
+library(bigmemory)
+
+#### Set working directory ####
+setwd("/Users/Morrison/Documents/Research Projects/Riparian Bayesian Network/climate_bayesian")
+
+#### Source common functions ####
+source('~/Documents/Research Projects/Riparian Bayesian Network/climate_bayesian/r_code/1-functions.R')
+
+hist_150 <- climate.hydrology("data/HAD_hist_150.csv")
+
+#### Add stage values (feet and cm) to data frame based on a regression equation ####
+# The regression equation was developed using historical gage data, and changes depending on the site location
+
+# SITE 1
+head_conv <- function(Q) 0.0011*Q
+
+# SITE 2
+# head_conv <- function(Q) 0.0011*Q
+
+# SITE 3
+# head_conv <- function(Q) 0.056*(Q^0.5612)
+
+# SITE 4
+# head_conv <- function(Q) 0.0014*(Q^0.99)
+
+# SITE 5
+# head_conv <- function(Q) 0.1123*(Q^0.4963)
+
+hist_150_stage <- stage(hist_150, head_conv)
+hist_150 <- data.frame(hist_150, hist_150_stage)
+
+#### Calculate recession rates ####
+# The 1-, 2-, ... , 7-day, 14-day recession rates were computed
+recess_1d <- recession.rate.forward(hist_150, 1)
+recess_2d <- recession.rate.forward(hist_150, 2)
+recess_3d <- recession.rate.forward(hist_150, 3)
+recess_4d <- recession.rate.forward(hist_150, 4)
+recess_5d <- recession.rate.forward(hist_150, 5)
+recess_6d <- recession.rate.forward(hist_150, 6)
+recess_7d <- recession.rate.forward(hist_150, 7)
+recess_14d <- recession.rate.forward(hist_150, 14)
+recess_30d <- recession.rate.forward(hist_150, 30)
+recess_45d <- recession.rate.forward(hist_150, 45)
+
+hist_150 <- cbind(hist_150, recess_1d, recess_2d, recess_3d, recess_4d, recess_5d, recess_6d, recess_7d, recess_14d, recess_30d, recess_45d)
+colnames(hist_150)[5:14] <- c("recess_1d", "recess_2d", "recess_3d", "recess_4d", "recess_5d", "recess_6d", "recess_7d", "recess_14d", "recess_30d", "recess_45d")
+
+#### Process Q-model bins ####
+# Model Q-bins were selected so that inundation areas increased by less than 15% between the discharge associated with each bin
+# q_bin <- c(719, 1209, 2100, 4000, 6296, 11300)
+# q_bin <- c(1000, 1200, 1400, 1600, 1800, 2000, 2200)
+q_bin <- c(1000, 1500, 2000, 2500, 3000, 3500, 4000)
+
+# Calculate probability of flooding in each bin for the entire year
+q_bin_prob <- q.prob(hist_150, q_bin)
+
+# Calculate probabilities in each bin according to discrete timing states
+# The discrete timing states are April-May, June-July, August-September
+hist_150_monthly <- subset.month(hist_150)
+
+apr_may <- list.to.df(hist_150_monthly[[1]])
+jun_jul <- list.to.df(hist_150_monthly[[2]])
+aug_sep <- list.to.df(hist_150_monthly[[3]])
+
+q_prob_apr_may <- q.prob(apr_may, q_bin)
+q_prob_jun_jul <- q.prob(jun_jul, q_bin)
+q_prob_aug_sep <- q.prob(aug_sep, q_bin)
+
+q_prob_all <- data.frame(q_prob_apr_may, q_prob_jun_jul, q_prob_aug_sep)
+# write.table(q_prob_all, "output/q_prob_all.txt", sep="\t")
+
+# Test calculations
+# R <- probspace(apr_may)
+# Y <- prob(R, cfs>=719)
+#
+# C <- count(apr_may$cfs>=719)
+
+#### Calculate probabilities for recession rates ####
+# Test calculations
+# S <- probspace(apr_may)
+# P <- prob(S, recess_6d>=6)
+# X <- S[["recess_1d"]]
+
+# Set discrete states for recession rates (in cm/day)
+recess_rates <- c(0, 1, 3, 6)
+
+# Calculate recession rate probabilities for April-May
+recess_1d_apr_may <- r.prob(apr_may, "recess_1d", recess_rates)
+recess_2d_apr_may <- r.prob(apr_may, "recess_2d", recess_rates)
+recess_3d_apr_may <- r.prob(apr_may, "recess_3d", recess_rates)
+recess_4d_apr_may <- r.prob(apr_may, "recess_4d", recess_rates)
+recess_5d_apr_may <- r.prob(apr_may, "recess_5d", recess_rates)
+recess_6d_apr_may <- r.prob(apr_may, "recess_6d", recess_rates)
+recess_7d_apr_may <- r.prob(apr_may, "recess_7d", recess_rates)
+recess_14d_apr_may <- r.prob(apr_may, "recess_14d", recess_rates)
+recess_30d_apr_may <- r.prob(apr_may, "recess_30d", recess_rates)
+recess_45d_apr_may <- r.prob(apr_may, "recess_45d", recess_rates)
+
+recess_all_apr_may <- cbind(recess_1d_apr_may, recess_2d_apr_may, recess_3d_apr_may, recess_4d_apr_may, recess_5d_apr_may, recess_6d_apr_may, recess_7d_apr_may, recess_14d_apr_may, recess_30d_apr_may, recess_45d_apr_may)
+colnames(recess_all_apr_may) <- c("recess_1d", "recess_2d", "recess_3d", "recess_4d", "recess_5d", "recess_6d", "recess_7d", "recess_14d", "recess_30d", "recess_45d")
+rownames(recess_all_apr_may) <- c("<0", "0-1", "1-3", "3-6", ">6")
+recess_all_apr_may <- t(recess_all_apr_may)
+
+# Calculate recession rate probabilities for June-July
+recess_1d_jun_jul <- r.prob(jun_jul, "recess_1d", recess_rates)
+recess_2d_jun_jul <- r.prob(jun_jul, "recess_2d", recess_rates)
+recess_3d_jun_jul <- r.prob(jun_jul, "recess_3d", recess_rates)
+recess_4d_jun_jul <- r.prob(jun_jul, "recess_4d", recess_rates)
+recess_5d_jun_jul <- r.prob(jun_jul, "recess_5d", recess_rates)
+recess_6d_jun_jul <- r.prob(jun_jul, "recess_6d", recess_rates)
+recess_7d_jun_jul <- r.prob(jun_jul, "recess_7d", recess_rates)
+recess_14d_jun_jul <- r.prob(jun_jul, "recess_14d", recess_rates)
+recess_30d_jun_jul <- r.prob(jun_jul, "recess_30d", recess_rates)
+recess_45d_jun_jul <- r.prob(jun_jul, "recess_45d", recess_rates)
+
+recess_all_jun_jul <- cbind(recess_1d_jun_jul, recess_2d_jun_jul, recess_3d_jun_jul, recess_4d_jun_jul, recess_5d_jun_jul, recess_6d_jun_jul, recess_7d_jun_jul, recess_14d_jun_jul, recess_30d_jun_jul, recess_45d_jun_jul)
+colnames(recess_all_jun_jul) <- c("recess_1d", "recess_2d", "recess_3d", "recess_4d", "recess_5d", "recess_6d", "recess_7d", "recess_14d", "recess_30d", "recess_45d")
+rownames(recess_all_jun_jul) <- c("<0", "0-1", "1-3", "3-6", ">6")
+recess_all_jun_jul <- t(recess_all_jun_jul)
+
+# Calculate recession rate probabilities for August-September
+recess_1d_aug_sep <- r.prob(aug_sep, "recess_1d", recess_rates)
+recess_2d_aug_sep <- r.prob(aug_sep, "recess_2d", recess_rates)
+recess_3d_aug_sep <- r.prob(aug_sep, "recess_3d", recess_rates)
+recess_4d_aug_sep <- r.prob(aug_sep, "recess_4d", recess_rates)
+recess_5d_aug_sep <- r.prob(aug_sep, "recess_5d", recess_rates)
+recess_6d_aug_sep <- r.prob(aug_sep, "recess_6d", recess_rates)
+recess_7d_aug_sep <- r.prob(aug_sep, "recess_7d", recess_rates)
+recess_14d_aug_sep <- r.prob(aug_sep, "recess_14d", recess_rates)
+recess_30d_aug_sep <- r.prob(aug_sep, "recess_30d", recess_rates)
+recess_45d_aug_sep <- r.prob(aug_sep, "recess_45d", recess_rates)
+
+recess_all_aug_sep <- cbind(recess_1d_aug_sep, recess_2d_aug_sep, recess_3d_aug_sep, recess_4d_aug_sep, recess_5d_aug_sep, recess_6d_aug_sep, recess_7d_aug_sep, recess_14d_aug_sep, recess_30d_aug_sep, recess_45d_aug_sep)
+colnames(recess_all_aug_sep) <- c("recess_1d", "recess_2d", "recess_3d", "recess_4d", "recess_5d", "recess_6d", "recess_7d", "recess_14d", "recess_30d", "recess_45d")
+rownames(recess_all_aug_sep) <- c("<0", "0-1", "1-3", "3-6", ">6")
+recess_all_aug_sep <- t(recess_all_aug_sep)
+
+recess_prob_all <- cbind(recess_all_apr_may, recess_all_jun_jul, recess_all_aug_sep)
+
+#### Populate network states based on scenarios ####
+# Discrete states (1, 2, 3) based on the timing
+timing_state <- timing.states(hist_150)
+
+# Discrete states (Y, N) based on inundation
+q1_state <- q.states(hist_150, q_bin, 1)
+q2_state <- q.states(hist_150, q_bin, 2)
+q3_state <- q.states(hist_150, q_bin, 3)
+q4_state <- q.states(hist_150, q_bin, 4)
+q5_state <- q.states(hist_150, q_bin, 5)
+q6_state <- q.states(hist_150, q_bin, 6)
+q7_state <- q.states(hist_150, q_bin, 7)
+
+# Discrete states based on recession rates
+recess_state <- recess.states(hist_150, recess_rates, "recess_14d")
---
+++ r_code/3-bayesian_network_construct.R
@@ -0,0 +1,71 @@
+#### Load packages ####
+# Load "bnlearn" package
+library(bnlearn)
+
+# Name network variables
+riparian_var <- c("TIMING", "FLOOD", "RECESSION", "HYDRO", "DEPTH", "POTENTIAL")
+
+# Create empty network
+riparian_dag <- empty.graph(riparian_var)
+
+# riparian_test <- model2network("[TIMING][DEPTH][FLOOD|TIMING][RECESSION|TIMING][HYDRO|FLOOD:RECESSION][POTENTIAL|HYDRO:DEPTH]")
+
+# Connect variables with directed arcs
+arcs(riparian_dag) <- matrix(
+	c("TIMING", "FLOOD", "TIMING", "RECESSION", "RECESSION", "HYDRO", "FLOOD", "HYDRO", "DEPTH", "POTENTIAL", "HYDRO", "POTENTIAL"),
+	ncol = 2, byrow = TRUE,
+	dimnames = list(c(), c("from", "to")))
+
+#### Discrete States ####
+# Set discrete states for each node
+
+# Key to timing statess: 1 = May-June; 2 = July-August, 3 = September-October
+dim_TIMING <- c(1, 2, 3)
+# Key to flooding states: Y = yes; N = no
+# dim_FLOOD <- c("Y", "N")
+dim_FLOOD <- c(1, 0)
+# dim_FLOOD <- c("TRUE", "FALSE") # USE FOR WINDOWS
+# Key to recession states: 1 = < 0cm/day 2 = 0-1 cm/day; 3 = 1-3 cm/day; 4 = 3-6 cm/day, 5 = > 6 cm/day
+dim_RECESSION <- c(1, 2, 3, 4, 5)
+# Key to hydrologic condition states: L=low; M=medium; H=high
+# dim_HYDRO <- c("L", "M", "H")
+dim_HYDRO <- c("L", "H")
+# Key to groundwater depth states: 1 = < 50 cm; 2 = 50-200 cm; 3 = > 200 cm
+dim_DEPTH <- c(1, 2, 3)
+# Key to recruitment potential states: L=low; M=medium; H=high
+# dim_POTENTIAL <- c("L", "M", "H")
+dim_POTENTIAL <- c("Y", "N")
+
+
+#### CPT Assignments ####
+cpt_TIMING <- matrix(c(0.65, 0.25, 0.1), ncol=3, dimnames=list(NULL, "TIMING"=dim_TIMING))
+cpt_FLOOD1 <- matrix(as.matrix(HB_q_prob_all[2, ]), ncol=3, dimnames=list("FLOOD"=dim_FLOOD, "TIMING"=dim_TIMING))
+cpt_FLOOD2 <- matrix(as.matrix(HB_q_prob_all[3, ]), ncol=3, dimnames=list("FLOOD"=dim_FLOOD, "TIMING"=dim_TIMING))
+cpt_FLOOD3 <- matrix(as.matrix(HB_q_prob_all[4, ]), ncol=3, dimnames=list("FLOOD"=dim_FLOOD, "TIMING"=dim_TIMING))
+cpt_FLOOD4 <- matrix(as.matrix(HB_q_prob_all[5, ]), ncol=3, dimnames=list("FLOOD"=dim_FLOOD, "TIMING"=dim_TIMING))
+cpt_FLOOD5 <- matrix(as.matrix(HB_q_prob_all[6, ]), ncol=3, dimnames=list("FLOOD"=dim_FLOOD, "TIMING"=dim_TIMING))
+cpt_FLOOD6 <- matrix(as.matrix(HB_q_prob_all[7, ]), ncol=3, dimnames=list("FLOOD"=dim_FLOOD, "TIMING"=dim_TIMING))
+cpt_FLOOD7 <- matrix(as.matrix(HB_q_prob_all[8, ]), ncol=3, dimnames=list("FLOOD"=dim_FLOOD, "TIMING"=dim_TIMING))
+cpt_RECESSION <- matrix(HB_recess_prob_all[8, ], ncol=3, dimnames=list("RECESSION"=dim_RECESSION, "TIMING"=dim_TIMING)) # Row 8 refers to the 14-day avg forward recession rate
+cpt_RECESSION <- cpt.roundoff(cpt_RECESSION)
+cpt_HYDRO <- matrix(c(1, 0, 1, 0, 0.05, 0.95, 1, 0, 0.1, 0.9, 1, 0, 0.3, 0.7, 1, 0, 0.9, 0.1, 1, 0))
+dim(cpt_HYDRO) <- c(2, 2, 5)
+dimnames(cpt_HYDRO) <- list("HYDRO"=dim_HYDRO, "FLOOD"=dim_FLOOD, "RECESSION"=dim_RECESSION)
+cpt_DEPTH <- matrix(c(0.33, 0.33, 0.34), ncol=3, dimnames=list(NULL, "DEPTH"=dim_DEPTH))
+# cpt_POTENTIAL <- matrix(c(0.3, 0.7, 0.4, 0.6, 0.5, 0.5, 0.5, 0.5, 0.6, 0.4, 0.7, 0.3, 0.3,0.7, 0.4, 0.6, 0.5, 0.5), ncol=3)
+# cpt_POTENTIAL <- matrix(c(0, 1, 0.4, 0.6, 0.5, 0.5, 0.5, 0.5, 0.6, 0.4, 0.7, 0.3, 0, 1, 0.4, 0.6, 0.5, 0.5), ncol=3)
+cpt_POTENTIAL <- matrix(c(0, 1, 0.7, 0.3, 0, 1, 0.9, 0.1, 0, 1, 0.7, 0.3), ncol=3)
+dim(cpt_POTENTIAL) <- c(2, 2, 3)
+dimnames(cpt_POTENTIAL) <- list("POTENTIAL"=dim_POTENTIAL, "HYDRO"=dim_HYDRO, "DEPTH"=dim_DEPTH)
+# cpt_POTENTIAL <- matrix(c(0.8, 0.1, 0.1, 0.3, 0.5, 0.2, 0.1, 0.3, 0.6, 0.75, 0.15, 0.1, 0.2, 0.4, 0.4, 0.05, 0.2, 0.75, 0.9, 0.05, 0.05, 0.3, 0.5, 0.2, 0.2, 0.4, 0.4))
+# dim(cpt_POTENTIAL) <- c(3, 3, 3)
+# dimnames(cpt_POTENTIAL) <- list("POTENTIAL"=dim_POTENTIAL, "HYDRO"=dim_HYDRO, "DEPTH"=dim_DEPTH)
+
+riparian.fit1 <- custom.fit(riparian_dag, dist=list(TIMING=cpt_TIMING, FLOOD=cpt_FLOOD1, RECESSION=cpt_RECESSION, HYDRO=cpt_HYDRO, DEPTH=cpt_DEPTH, POTENTIAL=cpt_POTENTIAL))
+riparian.fit2 <- custom.fit(riparian_dag, dist=list(TIMING=cpt_TIMING, FLOOD=cpt_FLOOD2, RECESSION=cpt_RECESSION, HYDRO=cpt_HYDRO, DEPTH=cpt_DEPTH, POTENTIAL=cpt_POTENTIAL))
+riparian.fit3 <- custom.fit(riparian_dag, dist=list(TIMING=cpt_TIMING, FLOOD=cpt_FLOOD3, RECESSION=cpt_RECESSION, HYDRO=cpt_HYDRO, DEPTH=cpt_DEPTH, POTENTIAL=cpt_POTENTIAL))
+riparian.fit4 <- custom.fit(riparian_dag, dist=list(TIMING=cpt_TIMING, FLOOD=cpt_FLOOD4, RECESSION=cpt_RECESSION, HYDRO=cpt_HYDRO, DEPTH=cpt_DEPTH, POTENTIAL=cpt_POTENTIAL))
+riparian.fit5 <- custom.fit(riparian_dag, dist=list(TIMING=cpt_TIMING, FLOOD=cpt_FLOOD5, RECESSION=cpt_RECESSION, HYDRO=cpt_HYDRO, DEPTH=cpt_DEPTH, POTENTIAL=cpt_POTENTIAL))
+riparian.fit6 <- custom.fit(riparian_dag, dist=list(TIMING=cpt_TIMING, FLOOD=cpt_FLOOD6, RECESSION=cpt_RECESSION, HYDRO=cpt_HYDRO, DEPTH=cpt_DEPTH, POTENTIAL=cpt_POTENTIAL))
+riparian.fit7 <- custom.fit(riparian_dag, dist=list(TIMING=cpt_TIMING, FLOOD=cpt_FLOOD7, RECESSION=cpt_RECESSION, HYDRO=cpt_HYDRO, DEPTH=cpt_DEPTH, POTENTIAL=cpt_POTENTIAL))
+
